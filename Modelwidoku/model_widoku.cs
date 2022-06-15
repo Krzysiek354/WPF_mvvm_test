722337fdcc94db81a1aa33224192b57af286d8f2
Krzysiek <krzwyr25@gmail.com>
2022-06-15 12:32:56 +0200

    mvvm

---
+++ Modelwidoku/model_widoku.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace wpf_mvvm_test.Modelwidoku
+{
+    using Model;
+    public class model_widoku : INotifyPropertyChanged
+    {
+        private model_mvvm model = new model_mvvm();
+
+        public double wartosc
+        {
+            get
+            { return model.wartosc; }
+
+            set { model.wartosc = value;
+                onPropertyChane(nameof(wartosc));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void onPropertyChane(string nazwa_wlasnosci)
+        {
+            if (PropertyChanged != null) PropertyChanged(this,new PropertyChangedEventArgs(nazwa_wlasnosci));
+        }
+    }
+}
